e3fcb5913cb15770428944b57940ef59b6e4a5cd
gajendra <davegk59@gmail.com>
2025-09-03 20:15:52 +0530

    read and delete functionality

--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
 let regForm = document.querySelector('.register-form'); 
 let allInput=regForm.querySelectorAll('INPUT');
 let closeBtn =document.querySelector(".btn-close");
+let regList =document.querySelector(".reg-list");
+
+
 let allRegData=[];
+
 let url = "";
 
 
 // Stored Data in localStorage 
-if(localStorage.getItem(allRegData) !=null){
+if(localStorage.getItem("allRegData") !=null){
     allRegData=JSON.parse(localStorage.getItem('allRegData'));
 }
 // console.log(allRegData); 
@@ -21,19 +25,74 @@ regForm.onsubmit = (e)=>{
     mobile:allInput[2].value,
     dob:allInput[3].value,
     passoword:allInput[4].value,
-    profile: url == "" ? "/img/download.png" : url
-});
-localStorage.setItem('allRegData',JSON.stringify(allRegData));
-swal("Data inserted successfully", "You clicked the button!", "success");
-closeBtn.click();
-regForm.reset("");
+    profile: url == "" ? "/img/download.png" : url, 
+    
+    });
+    localStorage.setItem('allRegData',JSON.stringify(allRegData));
+        Swal.fire({"Data inserted successfully", "You clicked the button!", "success"});
+        closeBtn.click();
+        regForm.reset("");
+        getRegData();
     }
     else
     {
         swal("Email already exists","failed","warning");
     }
+
 }
 
+const getRegData = ()=>{
+    regList.innerHTML="";
+    allRegData.forEach((data,index)=>{
+            regList.innerHTML += `
+            <tr>
+                <td>${index+1}</td>
+                <td>
+                <img src="${data.profile}" width="40" alt="image" style="width: 40px; border-radius: 50%;">
+                </td>
+                <td>${data.name}</td>
+                <td>${data.email}</td>
+                <td>${data.mobile}</td>
+                <td>${data.passoword}</td>
+                <td>
+                     <button data="${data}"  index="${index}" class="edit-btn btn btn-primary p-1 px-2"><i class="fa fa-edit"></i></button>
+                     <button index="${index}" class="del-btn btn btn-danger p-1 px-2"><i class="fa fa-trash"></i></button>
+                     
+                </td>
+            </tr>
+                  `
+    });
+   
+    action();
+}
+
+
+//Delete Coding
+const action=()=>{
+    //delet Coding 
+
+   let allDelBtn =document.querySelectorAll(".del-btn");
+   for(let btn of allDelBtn){
+    btn.onclick= async ()=>{
+        let index = btn.getAttribute("index");
+        let isConfirm = await confirm();
+        
+        if(isConfirm)
+        {
+            allRegData.splice(index,1);
+            localStorage.setItem("allRegData",JSON.stringify(allRegData));
+            getRegData();
+        }
+        // catch(error){
+        //     alert(error);
+        // }
+        
+        
+    }
+   }
+}
+
+getRegData();
 //Reading profile Photo
 allInput[5].onchange = ()=>{
     let freader = new FileReader();  
@@ -42,5 +101,36 @@ allInput[5].onchange = ()=>{
         url=e.target.result;
         console.log(url);
     }
-    }   
+}   
+
+//let confirm 
+
+const confirm =()=>{
+    return new Promise((resolve,reject)=>{
+        Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+        if (result) {
+            resolve(true);
+            Swal.fire({
+            title: "Deleted!",
+            text: "Your file has been deleted.",
+            icon: "success"
+            });
+            } else {
+                reject(false);
+                swal("Your imaginary file is safe");
+            }
+});
+    });
+
+}
+
+
     
